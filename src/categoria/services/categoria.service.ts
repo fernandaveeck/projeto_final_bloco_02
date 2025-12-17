1324d04bc98f6479360d05098d0edf2c57367499
fernanda <fernandaveeck@gmail.com>
2025-12-17 10:23:01 -0300

    feat: Método findAll e Categoria Controllers

--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -8,4 +8,8 @@ export class CategoriaService {
     constructor(
         @InjectRepository(Categoria) private categoriaRepository: Repository<Categoria>
     ) { }
+
+    async findAll(): Promise<Categoria[]> {
+        return await this.categoriaRepository.find();
+    }
 }
