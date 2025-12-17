acf5136258b877dedca489eb3ac0e050235cb379
fernanda <fernandaveeck@gmail.com>
2025-12-17 10:18:42 -0300

    feat: Criada Categoria Services

---
+++ src/categoria/services/categoria.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Categoria } from "../entities/categoria.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class CategoriaService {
+    constructor(
+        @InjectRepository(Categoria) private categoriaRepository: Repository<Categoria>
+    ) { }
+}
