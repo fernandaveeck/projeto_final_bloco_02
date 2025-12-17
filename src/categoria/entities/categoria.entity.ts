6d170e6284d3d457d4e5a66daa99a50de44dec21
fernanda <fernandaveeck@gmail.com>
2025-12-17 10:09:01 -0300

    feat: Criando a entidade Categoria

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name: "tb_categorias"})
+export class Categoria {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false})
+    descricao: string;
+
+    @UpdateDateColumn()
+    data: Date;
+
+}
