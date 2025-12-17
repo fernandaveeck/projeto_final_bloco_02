2867598b83de69e5ce87f89bff65dbd04a043ed1
fernanda <fernandaveeck@gmail.com>
2025-12-17 10:39:13 -0300

    feat: Método findById

--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Categoria } from "../entities/categoria.entity";
 import { Repository } from "typeorm";
 
 @Injectable()
 export class CategoriaService {
-    constructor(
-        @InjectRepository(Categoria) private categoriaRepository: Repository<Categoria>
-    ) { }
+    constructor(@InjectRepository(Categoria) private categoriaRepository: Repository<Categoria>) { }
 
     async findAll(): Promise<Categoria[]> {
         return await this.categoriaRepository.find();
+    }
+
+    async findById(id: number): Promise<Categoria> {
+        const categoria = await this.categoriaRepository.findOne({where: {id}})
+
+        if (!categoria)
+            throw new HttpException('Categoria não encontrada.', HttpStatus.NOT_FOUND);
+
+        return categoria;
     }
 }
